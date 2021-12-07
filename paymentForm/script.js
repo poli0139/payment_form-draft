5188818b45222480cc821b590253407fecfab651
Polina <p.artamonova@gmail.com>
2021-12-07 16:24:05 +0100

    started form

---
+++ paymentForm/script.js
@@ -0,0 +1,8 @@
+document
+  .querySelector(".cardNumber input")
+  .addEventListener("input", function (e) {
+    e.target.value = e.target.value
+      .replace(/[^\dA-Z]/g, "")
+      .replace(/(.{4})/g, "$1 ")
+      .trim();
+  });
