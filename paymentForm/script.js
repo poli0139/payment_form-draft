6fcc26cf0a583e8b66c45c30e8e138b313d3ffc5
Polina <p.artamonova@gmail.com>
2021-12-10 16:12:15 +0100

    done form

--- paymentForm/script.js
+++ paymentForm/script.js
@@ -9,13 +9,29 @@ document
 
 const monthInput = document.querySelector("#expiryMonth");
 monthInput.addEventListener("input", function (e) {
-  if (monthInput.value > 1) {
+  if (monthInput.value > 1 && monthInput.value.length == 1) {
+    monthInput.value = `0${monthInput.value}`;
     document.getElementById("expiryYear").focus();
+  } else if (monthInput.value.length == 2 && monthInput.value[1] <= 2) {
+    document.getElementById("expiryYear").focus();
+    console.log(monthInput.value[1]);
+  } else if (monthInput.value[1] > 2) {
+    document.querySelector(".monthErrMessage").classList.add("animateMessage");
   }
 });
 const yearInput = document.querySelector("#expiryYear");
 yearInput.addEventListener("input", function (e) {
   if (yearInput.value.length == 2) {
     document.querySelector("#securityCode").focus();
+  }
+});
+const cardNumberInput = document.querySelector("#cardNumber");
+document.querySelector("form").addEventListener("submit", function (e) {
+  if (cardNumberInput.value.length < 19) {
+    e.preventDefault();
+    console.log("invalid card");
+    document
+      .querySelector(".cardNumberErrMessage")
+      .classList.add("animateMessage");
   }
 });
